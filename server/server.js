1d4de8828c1760f015a5b948f68be83085e26d27
Andrew Ilijic <ailijic@gmail.com>
2017-01-12 23:28:23 -0500

    Modify server app to conform with JSlint

--- server/server.js
+++ server/server.js
@@ -1,16 +1,14 @@
-start();
-function start () {
-  'use strict';
+(function start() {
+    "use strict";
 
-  const express = require('express');
-  const app = express();
+    const express = require("express");
+    const app = express();
 
-  // app.use(bodyParser.json());
-  app.use(express.static('public'));
+    // app.use(bodyParser.json());
+    app.use(express.static("public"));
 
-  const login = require(`${__dirname}/routes/login`);
-  app.use('/login', login);
-  
-  app.listen(process.env.PORT || 8080);
-}
+    const login = require(`${__dirname}/routes/login`);
+    app.use("/login", login);
 
+    app.listen(process.env.PORT || 8080);
+}());
