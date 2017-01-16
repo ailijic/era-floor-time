2a87a445d161954897263444181e60abc40a6751
Andrew Ilijic <ailijic@gmail.com>
2017-01-16 04:40:28 -0500

    Add morgan to aid in server logging

--- server/server.js
+++ server/server.js
@@ -1,11 +1,18 @@
 (function start() {
     "use strict";
 
+    const morgan = require("morgan");
     const express = require("express");
     const login = require(`${__dirname}/routes/login`);
     const app = express();
 
+    /** Use Morgan to log requests */
+    app.use(morgan("dev"));
+
+    /** Add static sites */
     app.use(express.static("public"));
+
+    /** Add login Route */
     app.use("/login", login);
 
     app.listen(process.env.PORT || 8080);
