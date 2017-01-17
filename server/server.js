487cebe409358b0127fb0545beb73b07db108c97
Andrew Ilijic <ailijic@gmail.com>
2017-01-16 23:20:17 -0500

    Add filter to allow access control of endpoints

--- server/server.js
+++ server/server.js
@@ -4,6 +4,7 @@
     const morgan = require("morgan");
     const express = require("express");
     const login = require(`${__dirname}/routes/login`);
+    const jwt = require("jsonwebtoken");
     const app = express();
 
     /** Use Morgan to log requests */
@@ -12,8 +13,17 @@
     /** Add static sites */
     app.use(express.static("public"));
 
+    app.get("/dashboard", function (req, res) {
+        res.send("hello");
+    });
+
     /** Add login Route */
     app.use("/login", login);
 
+    app.use(authFilter);
+    
+    app.get("/secure", function (req, res) {
+        res.send(`Hello from the secure route!`);
+    });
     app.listen(process.env.PORT || 8080);
 }());
