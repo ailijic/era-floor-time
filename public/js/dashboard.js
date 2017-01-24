fe8fc2830fa8a49db460b7d4f46423dc5f8da934
Andrew Ilijic <ailijic@gmail.com>
2017-01-24 17:23:07 -0500

    Update dashboard to use json file

--- public/js/dashboard.js
+++ public/js/dashboard.js
@@ -1,41 +1,25 @@
-start()
-function start () {
-  'use strict'
+$(document).ready(start);
+function start() {
+    "use strict";
 
-  $(document).ready(function() {
-    // page is now ready, initialize the calendar...
-    /*
-    */
-    getShifts()
-    function getShifts () {
-      /* $.ajax({
-        type: "GET",
-        url: "/shifts",
-        datatype: "json",
-        contentType: "application/json; charset=utf-8",
-        success: (data) => {
-          console.log(data) // DELETE ME LATER */
-          $('#calendar').fullCalendar({
-            // put your options and callbacks here
-            // events: data.events,
-            events: [
-              {
-                title: 'Event1',
-                start: '2017-01-04'
-              },
-              {
-                title: 'Event2',
-                start: '2017-01-05'
-              }
-              // etc...
-            ],
-            
-            color: 'yellow',   // an option!
-            textColor: 'black' // an option!
-          });
-        //}
-      //})
+    $.getJSON("cal-data.json", renderCalendar);
+
+    function renderCalendar(eventsObj) {
+        $('#calendar').fullCalendar({
+            events: eventsObj.scheduleArray,
+            color: "yellow",
+            textColor: "black",
+            timezone: "local"
+        });
     }
-  })
+    // $('#calendar').fullCalendar({
+        // // put your options and callbacks here
+        // // events: data.events,
+        // events: [
+            // { title: 'Event1', start: '2017-01-04' },
+            // { title: 'Event2', start: '2017-01-05' }
+        // ],
+        // color: 'yellow',   // an option!
+        // textColor: 'black' // an option!
+    // });
 }
-
