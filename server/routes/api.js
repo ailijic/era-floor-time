611403dc8e2a09f27a77b6c81af4172d9da74944
Andrew Ilijic <ailijic@gmail.com>
2017-01-25 18:27:37 -0500

    Add create-Account route to server and api

--- server/routes/api.js
+++ server/routes/api.js
@@ -2,8 +2,21 @@
 
 const root = process.cwd();
 const express = require("express");
-const api = new express.Router();
+const api = express.Router();
 const db = require(`${root}/database/database`);
+const bodyParser = require("body-parser");
+const morgan = require("morgan");
 
-// Post request
-api.post("create-account", db.createAccount);
+api.use(morgan("dev"));
+api.use(bodyParser.json());
+api.use(bodyParser.urlencoded({ extended: true }));
+
+api.post("/create-account", (req, res) => {
+    console.log(req.body);
+    // db.user.add()
+    res
+        .json({ accountCreated: true })
+        .sendStatus(202);
+});
+
+module.exports = api;
--- server/server.js
+++ server/server.js
@@ -1,26 +1,28 @@
 "use strict";
 
-const   morgan  = require("morgan"),
-        express = require("express"),
-        login   = require(`${__dirname}/routes/login`),
-        api     = require(`${__dirname}/routes/api`),
-        app     = express();
+const   root        = process.cwd(),
+        authFilter  = require(`${root}/modules/auth-filter`),
+        login       = require(`${__dirname}/routes/login`),
+        api         = require(`${__dirname}/routes/api`),
+        morgan      = require("morgan"),
+        express     = require("express"),
+        app         = express();
 
-/** Use Morgan to log requests */
+/**
+ * Use Morgan to log requests
+ */
 app.use(morgan("dev"));
+
+/**
+ * Add static sites
+ */
+app.use(express.static("public"));
 
 app.use("/login", login);
 
 app.use("/api", api);
 
-/** Add static sites */
-app.use(express.static("public"));
-
-// app.get("/dashboard", function (req, res) {
-    // res.send("hello");
-// });
-
-// app.use(authFilter);
+app.use(authFilter);
 
 app.get("/secure", function (req, res) {
     res.send(`Hello from the secure route!`);
