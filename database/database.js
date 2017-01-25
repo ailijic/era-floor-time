a162f32e7ddff413a5e5641559b454a433022ef8
Andrew Ilijic <ailijic@gmail.com>
2017-01-25 11:07:40 -0500

    Refactor database

--- database/database.js
+++ database/database.js
@@ -1,41 +1,39 @@
-(function start() {
-    "use strict";
+"use strict";
 
-    const mongoose = require("mongoose");
-    mongoose.Promise = global.Promise;
-    const UserModel = require(`${__dirname}/models/user`);
+const mongoose = require("mongoose");
+mongoose.Promise = global.Promise;
+const UserModel = require(`${__dirname}/models/user`);
 
-    function add(credentials) {
-        const newRecord = new UserModel(credentials);
-        return newRecord.save().exec();
+function add(credentials) {
+    const newRecord = new UserModel(credentials);
+    return newRecord.save().exec();
+}
+
+function isMatch(userInput, dbRecord) {
+    let ret = false;
+    if (dbRecord === null) {
+        console.log("AUTH: Record not found");
+    } else if (userInput.username === dbRecord.username &&
+            userInput.password === dbRecord.password) {
+        ret = true;
     }
+    return ret;
+}
 
-    function isMatch(userInput, dbRecord) {
-        let ret = false;
-        if (dbRecord === null) {
-            console.log("AUTH: Record not found");
-        } else if (userInput.username === dbRecord.username &&
-                userInput.password === dbRecord.password) {
-            ret = true;
-        }
-        return ret;
-    }
+function verify(credentials) {
+    return UserModel.findOne(credentials).exec()
+        .then(function (dbRecord) {
+            return isMatch(credentials, dbRecord);
+        })
+        .catch(function (err) {
+            console.log(`ERR-FUNC(verify): ${err}`);
+        });
+}
 
-    function verify(credentials) {
-        return UserModel.findOne(credentials).exec()
-            .then(function (dbRecord) {
-                return isMatch(credentials, dbRecord);
-            })
-            .catch(function (err) {
-                console.log(`ERR-FUNC(verify): ${err}`);
-            });
-    }
+const user = {
+    add,
+    verify
+};
 
-    const user = {
-        add,
-        verify
-    };
-
-    mongoose.connect("mongodb://localhost/era-shift-db-dev");
-    module.exports = {user};
-}());
+mongoose.connect("mongodb://localhost/era-shift-db-dev");
+module.exports = {user};
