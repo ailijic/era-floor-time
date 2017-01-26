99196b9f2c5f7b1d2b720269ef69b12ecaef11b1
Andrew Ilijic <ailijic@gmail.com>
2017-01-26 15:16:32 -0500

    Change POST login path to be /api/user/login

--- public/js/login.js
+++ public/js/login.js
@@ -1,35 +1,33 @@
 /* eslint-env jquery */
 
-function start() {
-    'use strict';
+start();
+function start () {
+  'use strict';
 
-    const response = {
-        process: function (data, status) {
-            $(".msg").children().remove();
-            $(".msg").append(
-                $("<p></p>").text(data.message)
-            );
-        }
-    };
-
-    $("form").submit((event) => {
-        const username = $("#username").val();
-        const password = $("#password").val();
-        const loginObj = {
-            username,
-            password
-        };
-
-        event.preventDefault();
-
-        $.post({
-            url: '/api/user/login',
-            data: JSON.stringify(loginObj),
-            dataType: 'JSON',
-            contentType: 'application/json; charset=utf-8',
-                success: response.process
-        });
+  $(document).ready(() => {
+    $('form').submit(event => {
+      event.preventDefault();
+      const username = $('#username').val();
+      const password = $('#password').val();
+      const loginObj = {
+        username,
+        password
+      };
+      $.post({
+        url: '/login',
+        data: JSON.stringify(loginObj),
+        dataType: 'JSON',
+        contentType: 'application/json; charset=utf-8',
+        success: response.process
+      });
     });
+  });
+  const response = {
+    process: function (data, status) {
+      $('#login').append(`
+        <p> Status: ${status} </p>
+        <p> Data: ${data} </p>
+      `);
+    }
+  };
 }
-
-$(document).ready(start);
