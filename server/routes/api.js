650e0d2c2115575397d6a8b790b25da02b0fe853
Andrew Ilijic <ailijic@gmail.com>
2017-01-26 15:17:30 -0500

    Add /user/login to the api route

--- server/routes/api.js
+++ server/routes/api.js
@@ -5,6 +5,23 @@ const express = require("express");
 const api = express.Router();
 const db = require(`${root}/database/database`);
 const bodyParser = require("body-parser");
+const login = {
+    fail: {
+        success: false,
+        message: "Authentication Failed"
+    },
+    success: {
+        success: true,
+        message: "Login Granted",
+        // TODO: Add token gen
+        token: "FEDCBA9876543210"   // token.gen() // Create token
+    },
+    status: {
+        fail: 200,      // 200: OK
+        success: 202    // 202: Accepted
+    }
+};
+
 
 api.use(bodyParser.json());
 api.use(bodyParser.urlencoded({ extended: true }));
@@ -32,6 +49,22 @@ api.post("/create-account", (req, res) => {
                     message: "Server Unable to Create Account"
                 });
         });
+});
+
+api.post("/user/login", (req, res) => {
+    db.user.verify(req.body)
+        .then((isValidUser) => {
+            if (isValidUser === true) {
+                res
+                    .status(login.status.success)
+                    .json(login.success);
+            } else {
+                res
+                    .status(login.status.fail)
+                    .json(login.fail);
+            }
+        })
+        .catch(console.log);    
 });
 
 module.exports = api;
