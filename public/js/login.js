286eecc32c801e4c3fa59100198292edb7fa5766
Andrew Ilijic <ailijic@gmail.com>
2017-01-26 15:17:30 -0500

    Add /user/login to the api route

--- public/js/login.js
+++ public/js/login.js
@@ -1,33 +1,36 @@
 /* eslint-env jquery */
 
-start();
-function start () {
-  'use strict';
+function start() {
+    'use strict';
 
-  $(document).ready(() => {
-    $('form').submit(event => {
-      event.preventDefault();
-      const username = $('#username').val();
-      const password = $('#password').val();
-      const loginObj = {
-        username,
-        password
-      };
-      $.post({
-        url: '/login',
-        data: JSON.stringify(loginObj),
-        dataType: 'JSON',
-        contentType: 'application/json; charset=utf-8',
-        success: response.process
-      });
+    const response = {
+        process: function (data, status) {
+            $(".msg").children().remove();
+            $(".msg").append(
+                $("<p></p>").text(data.message)
+            );
+        }
+    };
+
+    $("form").submit((event) => {
+        const username = $("#username").val();
+        const password = $("#password").val();
+        const loginObj = {
+            username,
+            password
+        };
+
+        event.preventDefault();
+
+        $.post({
+            url: '/api/user/login',
+            data: JSON.stringify(loginObj),
+            dataType: 'JSON',
+            contentType: 'application/json; charset=utf-8',
+                success: response.process
+        });
     });
-  });
-  const response = {
-    process: function (data, status) {
-      $('#login').append(`
-        <p> Status: ${status} </p>
-        <p> Data: ${data} </p>
-      `);
-    }
-  };
 }
+
+$(document).ready(start);
+
