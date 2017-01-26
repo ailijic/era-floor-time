9621b269deddef4cb138709b1d7ef8a0e61b73ce
Andrew Ilijic <ailijic@gmail.com>
2017-01-26 10:27:06 -0500

    Add email and name fields to the database User model

--- database/models/user.js
+++ database/models/user.js
@@ -1,17 +1,15 @@
-// models/shift.js
+"use strict";
 
-start();
-function start () {
-  'use strict';
+const mongoose = require("mongoose");
 
-  const mongoose = require('mongoose');
-  
-  const ItemSchema = new mongoose.Schema({
-    username: { type: String, required: true, unique: true },
-    password: { type: String, required: true },
-  });
-  
-  const User = mongoose.model('User', ItemSchema);
-  
-  module.exports = User;
-}
+const ItemSchema = new mongoose.Schema({
+    username:       { type: String, required: true, unique: true },
+    password:       { type: String, required: true },
+    email:          { type: String, required: false },
+    firstName:      { type: String, required: false },
+    lastName:       { type: String, required: false }
+});
+
+const User = mongoose.model("User", ItemSchema);
+
+module.exports = User;
