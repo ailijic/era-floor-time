32489d161bdc73fc1db37f7b1fab3faf96fe4156
Andrew Ilijic <ailijic@gmail.com>
2017-01-26 20:41:17 -0500

    Add ability to toggle availability

--- public/js/submit-availability.js
+++ public/js/submit-availability.js
@@ -5,13 +5,24 @@ function start() {
     const nextMonth = (currentDate) => currentDate.getMonth() + 1;
 
     function renderCalendar(eventsObj) {
-        console.log(eventsObj);
         $('#calendar').fullCalendar({
             events: eventsObj,
             displayEventEnd: true,
             defaultDate: moment().add(1, "months"),
+            eventClick: toggleColor,
             timezone: "local"
         });
+    }
+
+    function toggleColor(event, jsEvent, view) {
+        if (event.color === "red") {
+            event.color = "green";
+            event.title = "Available";
+        } else if (event.color === "green") {
+            event.color = "red";
+            event.title = "Unavailable";
+        }
+        $('#calendar').fullCalendar("rerenderEvents");
     }
 
     $.getJSON("shift-template.json", renderCalendar);
