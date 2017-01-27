d7938cd0de6bea530c8a7ac604103ede17f7587f
Andrew Ilijic <ailijic@gmail.com>
2017-01-27 05:49:38 -0500

    Add submit button

--- public/js/submit-availability.js
+++ public/js/submit-availability.js
@@ -4,6 +4,13 @@ function start() {
     const currentDate = new Date();
     const nextMonth = (currentDate) => currentDate.getMonth() + 1;
 
+    function genSubmitButton() {
+        $("#submit")
+            .append(`<button type = "button"> Submit Floor Time </button>`);
+        $("#submit").css("text-align", "center");
+        // TODO: Add event click handler
+    }
+
     function renderCalendar(eventsObj) {
         $('#calendar').fullCalendar({
             events: eventsObj,
@@ -12,6 +19,16 @@ function start() {
             eventClick: toggleColor,
             timezone: "local"
         });
+        reRender();
+    }
+
+    function reRender() {
+        $("#calendar").fullCalendar("rerenderEvents");
+        $(".fc-event").css("height", "3em");
+        $(".fc-day-grid-event").css("white-space", "normal");
+        $(".fc-content").css("white-space", "normal");
+        $("#calendar").css("margin", "0 auto")
+        $("#calendar").css("max-width", "650px")
     }
 
     function toggleColor(event, jsEvent, view) {
@@ -22,10 +39,11 @@ function start() {
             event.color = "red";
             event.title = "Unavailable";
         }
-        $('#calendar').fullCalendar("rerenderEvents");
+        reRender();
     }
 
     $.getJSON("shift-template.json", renderCalendar);
+    genSubmitButton();
 }
 
 $(document).ready(start);
