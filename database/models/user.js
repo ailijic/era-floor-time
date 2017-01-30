ae80f284c4ea8754da06027057c941c8a37ad0ea
Andrew Ilijic <ailijic@gmail.com>
2017-01-30 08:50:48 -0500

    Add availability field ([Boolean]) to user model

--- database/models/user.js
+++ database/models/user.js
@@ -3,11 +3,12 @@
 const mongoose = require("mongoose");
 
 const ItemSchema = new mongoose.Schema({
-    username:       { type: String, required: true, unique: true },
-    password:       { type: String, required: true },
-    email:          { type: String, required: false },
-    firstName:      { type: String, required: false },
-    lastName:       { type: String, required: false }
+    username:       { type: String,     required: true, unique: true },
+    password:       { type: String,     required: true },
+    email:          { type: String,     required: false },
+    firstName:      { type: String,     required: false },
+    lastName:       { type: String,     required: false },
+    availability:   { type: [Boolean],  required: false }
 });
 
 const User = mongoose.model("User", ItemSchema);
