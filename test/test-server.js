4202fdabf07f326be45ea0b5ad5b8ab6ea632d8a
Andrew Ilijic <ailijic@gmail.com>
2017-02-08 17:49:20 -0500

    Initial commit and setup for app testing

---
+++ test/test-server.js
@@ -0,0 +1,12 @@
+// Require
+require("rootpath")();
+const chai require("chai");
+const chaiHttp = require("chai-http");
+const server = require("server/server");
+
+// Constants
+const should =chai.should();
+const expect = chai.expect;
+
+// Main
+describe()
