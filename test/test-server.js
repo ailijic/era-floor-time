9a7d3a30d896fb74f1759154c63690541491720e
Andrew Ilijic <ailijic@gmail.com>
2017-02-10 10:31:53 -0500

    Update tests

--- test/test-server.js
+++ test/test-server.js
@@ -14,10 +14,43 @@ const expect = chai.expect;
 
 // Main
 chai.use(chaiHttp);
-describe("Server", () => {
+describe("Server:", () => {
     beforeEach((done) => { // Empty the DB before each test
         userDB.remove({}, (err) => {
             done();
+        });
+    });
+
+    describe("HTTP GET /login", () => {
+        it("should return status 200", (done) => {
+            chai.request(server)
+                .get("/login")
+                .end((err, res) => {
+                    res.should.have.status(200);
+
+                    done();
+                });
+        });
+    });
+});
+
+describe("API:", () => {
+    describe("POST: /api/user/login", () => {
+        it("should redirect with status 303", (done) => {
+            chai.request(server)
+                .post("/api/user/login")
+                .redirects(0)
+                .field("username", "a")
+                .field("password", "a")
+                .end((err, res) => {
+                    // console.log(res);
+                    res.should.have.headers;
+                    res.should.redirect;
+                    res.should.have.status(303);
+                    res.should.redirectTo("/login");
+
+                    done();
+                });
         });
     });
 });
